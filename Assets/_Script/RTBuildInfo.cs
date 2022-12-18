27af4e79cea959f33a2acd69cc6ebaa4f689eaa9
Seth Robinson <seth@rtsoft.com>
2022-12-18 17:11:51 +0900

    Timestamp (this gets auto written when I do a release build)

--- Assets/_Script/RTBuildInfo.cs
+++ Assets/_Script/RTBuildInfo.cs
@@ -1,1 +1,1 @@
-public static class RTBuildInfo { public static string Timestamp = "Wed 12/14/2022 15:24:57.88".Substring(0,20);} 
+public static class RTBuildInfo { public static string Timestamp = "Sun 12/18/2022 17:08:28.15".Substring(0,20);} 
