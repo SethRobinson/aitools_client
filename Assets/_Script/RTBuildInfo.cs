955a394a2cd4608b08f4c5151259d174bd57151c
Seth Robinson <seth@rtsoft.com>
2026-06-21 19:32:36 +0900

    Update Unity generated assets

--- Assets/_Script/RTBuildInfo.cs
+++ Assets/_Script/RTBuildInfo.cs
@@ -1,1 +1,1 @@
-public static class RTBuildInfo { public static string Timestamp = "Sun 06/21/2026 12:35:24.77".Substring(0,20);} 
+public static class RTBuildInfo { public static string Timestamp = "Sun 06/21/2026 17:54:31.68".Substring(0,20);} 
