151e8b1c0dcf384f598255d7b565ade7b9cff0ac
Seth Robinson <seth@rtsoft.com>
2026-07-10 14:34:09 +0900

    * Misc

--- Assets/_Script/RTBuildInfo.cs
+++ Assets/_Script/RTBuildInfo.cs
@@ -1,1 +1,1 @@
-public static class RTBuildInfo { public static string Timestamp = "Fri 07/10/2026 13:47:03.33".Substring(0,20);} 
+public static class RTBuildInfo { public static string Timestamp = "Fri 07/10/2026 14:22:11.47".Substring(0,20);} 
